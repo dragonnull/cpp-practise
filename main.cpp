0a0931a0fca07e0daf3c64c44cfa6b3a857ea265
dragonnull <wwt67@qq.com>
2023-03-02 17:51:20 +0800

    程序员面试金典面试题 03.03. 堆盘子

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <string>
 
-int main() {
+using std::string;
+
+// class Solution
+// {
+// public:
+//     string printBin(double num)
+//     {
+//         string res = "0.";
+//         while (res.size() <= 32 && num != 0)
+//         {
+//             num *= 2;
+//             int digit = num;
+//             res.push_back('0' + digit);
+//             num -= digit;
+//         }
+//         return res.size() <= 32 ? res : "ERROR";
+//     }
+// };
+
+int main()
+{
     std::cout << "Hello, world!" << std::endl;
 }
