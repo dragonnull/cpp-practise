47e76a5bde74e4d08edba04fdfdafbfa4f157a5d
Ammy556 <wwt67@qq.com>
2023-03-09 18:00:11 +0800

    面试题 05.04. 下一个数位运算处理，巧妙的转换

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <bitset>
 #include <iostream>
 #include <set>
 #include <sstream>
@@ -6,14 +7,20 @@
 #include <vector>
 #include <unordered_map>
 
-using std::min;
-using std::set;
-using std::stack;
-using std::string;
-using std::stringstream;
-using std::vector;
-using std::unordered_map;
-using std::to_string;
+//using std::bitset;
+//using std::cout;
+//using std::count;
+//using std::endl;
+//using std::max;
+//using std::min;
+//using std::set;
+//using std::stack;
+//using std::string;
+//using std::stringstream;
+//using std::vector;
+//using std::unordered_map;
+//using std::to_string;
+using namespace std;
 
 struct TreeNode {
     int val;
@@ -25,25 +32,60 @@ struct TreeNode {
 
 class Solution {
 public:
-    TreeNode *inorderSuccessor(TreeNode *root, TreeNode *p) {
-        if (root == nullptr) return nullptr;
-        if (root->val <= p->val) return inorderSuccessor(root->right, p);
-        TreeNode *ans = inorderSuccessor(root->left, p);
-        return ans == nullptr ? root : ans;
-    }
+    vector<int> findClosedNumbers(int num) {
+        bitset<32> small(num);
+        bitset<32> bigger(num);
 
-    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
-        if (root == nullptr || root == p || root == q) return root;
-        TreeNode *left = lowestCommonAncestor(root->left, p, q);
-        TreeNode *right = lowestCommonAncestor(root->right, p, q);
-        if (left && right)
-            return root;
-        return left ? left : right;
-    }
+        int s = -1;
+        // small, 10 转 01，1移到左侧
+        // 找较小值，10 转 01，然后处理交换位置的右侧
+        // 处理：把所有的1尽量左靠，0靠右，值会尽量大，只比交换前小一点
+        for (int i = 1; i < 32; i++) {
+            if (small[i] == 1 && small[i - 1] == 0) {
+                cout << i << endl;
+                small.flip(i);
+                small.flip(i - 1);
+                for (int low = 0, high = i - 2; low < high;) {
+                    while (low < high && small[low] == 0) low++;
+                    while (low < high && small[high] == 1) high--;
+                    small.flip(low);
+                    small.flip(high);
+                }
+                s = (int)small.to_ulong();
+                break;
+            }
+        }
 
+        // bigger, 01转10，1移到最右侧
+        // 找较大值同理，01 转 10，然后处理交换位置的右侧
+        // 处理：把所有的1尽量靠右，0靠左，值会尽量小，只比交换前大一点
+        int b = -1;
+        for (int i = 1; i < 32; i++) {
+            if (bigger[i] == 0 && bigger[i - 1] == 1) {
+                bigger.flip(i);
+                bigger.flip(i - 1);
+
+                for (int low = 0, high = i - 2; low < high;) {
+                    while (low < high && bigger[low] == 1) low++;
+                    while (low < high && bigger[high] == 0) high--;
+                    bigger.flip(low);
+                    bigger.flip(high);
+                }
+                b = (int)bigger.to_ulong();
+                break;
+            }
+        }
+
+        return {b, s};
+    }
 };
 
 
 int main() {
-
+    Solution s;
+    int test = 4;
+    vector<int> res = s.findClosedNumbers(test);
+    cout << test << ": " << bitset<32>(test) << endl;
+    cout << res[0] << ": " << bitset<32>(res[0]) << endl;
+    cout << res[1] << ": " << bitset<32>(res[1]) << endl;
 }
