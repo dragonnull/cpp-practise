688860bb1e59559a5cf8cb0f75d551c1620385c0
Ammy556 <wwt67@qq.com>
2023-03-12 20:05:05 +0800

    面试题 17.05. 字母与数字 前缀和

--- main.cpp
+++ main.cpp
@@ -32,60 +32,45 @@ struct TreeNode {
 
 class Solution {
 public:
-    vector<int> findClosedNumbers(int num) {
-        bitset<32> small(num);
-        bitset<32> bigger(num);
-
-        int s = -1;
-        // small, 10 转 01，1移到左侧
-        // 找较小值，10 转 01，然后处理交换位置的右侧
-        // 处理：把所有的1尽量左靠，0靠右，值会尽量大，只比交换前小一点
-        for (int i = 1; i < 32; i++) {
-            if (small[i] == 1 && small[i - 1] == 0) {
-                cout << i << endl;
-                small.flip(i);
-                small.flip(i - 1);
-                for (int low = 0, high = i - 2; low < high;) {
-                    while (low < high && small[low] == 0) low++;
-                    while (low < high && small[high] == 1) high--;
-                    small.flip(low);
-                    small.flip(high);
+    vector<string> findLongestSubarray(vector<string> &array) {
+        int n = array.size();
+        int sum = 0;
+        unordered_map<int, int> record;
+        record[0] = -1;
+        int index = 0;
+        int len = 0;
+        for (int i = 0; i < n; ++i) {
+            if (array[i][0] >= '0' && array[i][0] <= '9') {
+                ++sum;
+            } else {
+                --sum;
+            }
+            auto it = record.find(sum);
+            if (it == record.end()) {
+                record[sum] = i;
+            } else {
+                if (len < i - it->second) {
+                    index = it->second + 1;
+                    len = i - it->second;
                 }
-                s = (int)small.to_ulong();
-                break;
             }
         }
-
-        // bigger, 01转10，1移到最右侧
-        // 找较大值同理，01 转 10，然后处理交换位置的右侧
-        // 处理：把所有的1尽量靠右，0靠左，值会尽量小，只比交换前大一点
-        int b = -1;
-        for (int i = 1; i < 32; i++) {
-            if (bigger[i] == 0 && bigger[i - 1] == 1) {
-                bigger.flip(i);
-                bigger.flip(i - 1);
-
-                for (int low = 0, high = i - 2; low < high;) {
-                    while (low < high && bigger[low] == 1) low++;
-                    while (low < high && bigger[high] == 0) high--;
-                    bigger.flip(low);
-                    bigger.flip(high);
-                }
-                b = (int)bigger.to_ulong();
-                break;
-            }
+        if (len == 0) {
+            return vector<string>();
+        } else {
+            return vector<string>(array.begin() + index, array.begin() + index + len);
         }
-
-        return {b, s};
     }
 };
 
 
 int main() {
     Solution s;
-    int test = 4;
-    vector<int> res = s.findClosedNumbers(test);
-    cout << test << ": " << bitset<32>(test) << endl;
-    cout << res[0] << ": " << bitset<32>(res[0]) << endl;
-    cout << res[1] << ": " << bitset<32>(res[1]) << endl;
+    vector<string> test = {"A", "1"/*, "B", "C", "D", "2", "3", "4", "E", "5", "F", "G", "6", "7", "H", "I", "J", "K",
+                           "L", "M"*/};
+    vector<string> ans = s.findLongestSubarray(test);
+    for (const auto &str : ans) {
+        cout << str << "\t";
+    }
+    cout << endl;
 }
