5fdc83f577b9a6f47625a482e83772ace229e8a6
Ammy556 <wwt67@qq.com>
2023-03-16 14:43:37 +0800

    2488. 统计中位数为 K 的子数组 大于小于转换为1和-1

--- main.cpp
+++ main.cpp
@@ -32,46 +32,22 @@ struct TreeNode {
 
 class Solution {
 public:
-    int rows;
-    int cols;
-    bool find = false;
-
-    vector<vector<int>> pathWithObstacles(vector<vector<int>> &obstacleGrid) {
-        vector<vector<int>> path;
-        rows = obstacleGrid.size();
-        cols = obstacleGrid[0].size();
-        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
-        if (obstacleGrid[0][0] || obstacleGrid[rows - 1][cols - 1])
-            return path;
-        dfs(obstacleGrid, path, visited, 0, 0);
-        return path;
+    int countSubarrays(vector<int> &nums, int k) {
+        int pos = find(nums.cbegin(), nums.cend(), k) - nums.cbegin();
+        unordered_map<int, int> count{{0, 1}};
+        for (int i = pos - 1, x = 0; i >= 0; --i) {
+            x += nums[i] < k ? 1 : -1;
+            ++count[x];
+        }
+        int ans = count[0] + count[-1];
+        int len = nums.size();
+        for (int i = pos + 1, x = 0; i < len; ++i) {
+            x += nums[i] > k ? 1 : -1;
+            ans += count[x] + count[x - 1];
+        }
+        return ans;
     }
 
-    bool
-    dfs(vector<vector<int>> &obstacleGrid, vector<vector<int>> &path,
-        vector<vector<bool>> &visited, int row, int col) {
-        if (find) return true;
-        if (row >= rows || col >= cols || visited[row][col])
-            return false;
-        visited[row][col] = true;
-        path.push_back({row, col});
-        if ((row == rows - 1) && (col == cols - 1)) {
-            find = true;
-            return true;
-        }
-        if (row + 1 < rows && obstacleGrid[row + 1][col] != 1) {
-            if (dfs(obstacleGrid, path, visited, row + 1, col)) {
-                return true;
-            }
-        }
-        if (col + 1 < cols && obstacleGrid[row][col + 1] != 1) {
-            if (dfs(obstacleGrid, path, visited, row, col + 1)) {
-                return true;
-            }
-        }
-        path.pop_back();
-        return false;
-    }
 };
 
 
